1164405369786cba204450659b867977884aaeeb
Shilin Wu <shwu@confluent.io>
2022-03-09 15:04:19 +0800

    Initial

--- get-topics.sh
+++ get-topics.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath build/libs/kafka_topic_configs-1.0-SNAPSHOT.jar ListTopicsKt -t topics.txt -c client.properties
+java -classpath kafka_topic_configs-1.0.0.jar ListTopicsKt -t topics.txt -c client.properties
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -jar build/libs/kafka_topic_configs-1.0-SNAPSHOT.jar -m min.insync.replicas=2 -m compression.type=producer  -t topics.txt -c client.properties --no
+java -jar kafka_topic_configs-1.0.0.jar -m min.insync.replicas=2 -m compression.type=producer  -t topics.txt -c client.properties --no
 
 
